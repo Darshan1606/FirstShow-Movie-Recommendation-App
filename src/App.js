df0050d6b546d1c3d096b02154493c81940491af
unknown <darshantarsariya17@gmail.com>
2022-11-10 21:24:40 +0530

    firstshow commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import SignIn from "./components/authentication/signin";
+// import SignUp from "./components/authentication/signup";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// import Home from "./components/home/Home"
+// import Marvelchar from "./components/marvelchar/Marvelchar";
+import Movies from "./components/movies/Movies";
+// import Marvelseries from "./components/marvelseries/Marvelseries";
+// import Marvelstories from "./components/marvelstories/Marvelstories";
+// import axios from "axios";
+import React from "react";
+
+//967780d831a3680144c23bf9c5fa53af
+//const hash = "967780d831a3680144c23bf9c5fa53af";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/"element={<Movies />}  />
+          {/* <Route path="/charcters" element={<Marvelchar />} />
+          <Route path="/comics" element={<movies />} />
+          <Route path="/series" element={<Marvelseries />} />
+          <Route path="/stories" element={<Marvelstories />} /> */}
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
